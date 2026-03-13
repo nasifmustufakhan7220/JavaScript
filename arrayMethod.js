389bce7213d44be238f56f073c67edec83c3c870
Nasif Mustufa Khan <nasifmustufakhan7220@gmail.com>
2026-03-14 03:34:40 +0600

    updated

--- arrayMethod.js
+++ arrayMethod.js
@@ -9,6 +9,74 @@
 // const fruitsNew = furits.copyWithin(2, 0);
 // console.log(fruitsNew);
 
-const furits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
-const fruitsNew = furits.copyWithin(2, 0);
-console.log(fruitsNew);
+// const furits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// const fruitsNew = furits.copyWithin(2, 0);
+// console.log(fruitsNew);
+
+
+// const fruit = ["Banana", "Orange", "Apple", "Mango"];
+
+// let myList = fruits.toString();
+// console.log(myList);
+
+
+// let fruit = fruits.at(2);
+// console.log(fruit);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "kiwi"];
+
+// console.log(fruits.join(' * '));
+
+
+// let result = fruits.pop();
+// let result = fruits.push("Kiwi");
+// let result = fruits.shift();
+// let result = fruits.unshift("Kiwi");
+// let result = Array.isArray(fruits);
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "kiwi"];
+
+// let result = fruits.copyWithin(target, start, end);
+// console.log(fruits);
+
+// const myArr = [[1, 2], [3, 4], [5, 6]];
+
+// let result = myArr.flat();
+// console.log(result.length);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let result = fruits.splice(1, 2, "Lemon", "Jackfruits");
+// console.log(`
+// [${fruits}]
+// [${result}]`);
+
+// let slides = ["img1", "img2", "img3", "img4", "img5"];
+
+// let target = 2;
+// let start = 0;
+// let end = 2;
+// let result = slides.copyWithin(target, start, end);
+// console.log(result);
+
+// let slides = ["img1", "img2", "img3", "img4", "img5"];
+
+// let whereAdded = 3;
+// let howManyRemove = 3;
+// let result = slides.splice(whereAdded, howManyRemove, "image1", "Image2");
+// console.log(slides, result);
+
+// let slides = ["img1", "img2", "img3", "img4", "img5"];
+
+// let result = slides.toSpliced(3, 1);
+// console.log(slides, result);
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+
+// const citrus = fruits.slice(1);
+
+// console.log(fruits, citrus);
+
+// const citrus = fruits.slice(1, 2);
+const citrus = fruits.slice(1);
+console.log(fruits, citrus);
