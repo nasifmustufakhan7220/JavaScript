66b204faeed0c679ac3eef479595703bb95692ea
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-04-02 16:31:28 +0600

    updated

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,23 @@
+const fruitsTitle = document.getElementById("fruits-title");
+const places = document.getElementsByClassName('importan-places');
+for(const place of places){
+   place.innerText = "Netrakona";
+   break;
+}
 
+fruitsTitle.innerText = "Foods I like most";
+
+for(const place of places){
+    place.style.color = "tomato"
+}
+
+const changeColor = (newColor) =>{
+    const element = document.getElementById('para');
+    element.style.color = newColor;
+}
+
+document.querySelectorAll("button").forEach((button)=>{
+    button.addEventListener("click", (event)=>{
+        changeColor(event.target.textContent);
+    })
+});
