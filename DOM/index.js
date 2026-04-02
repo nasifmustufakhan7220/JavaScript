5215e79e93a1cf5a30c28da1fe71084583d7c89e
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-04-02 23:08:47 +0600

    updated

--- DOM/index.js
+++ DOM/index.js
@@ -1,15 +1,40 @@
 const fruitsTitle = document.getElementById("fruits-title");
-const places = document.getElementsByClassName('importan-places');
-for(const place of places){
-   place.innerText = "Netrakona";
-   break;
+const places = document.querySelectorAll("#favourite-place li");
+const singelPlaceRemove = document.querySelectorAll("#favourite-place li")[3];
+const place = document.getElementsByClassName('importan-places')[0];
+const singlePlace = document.querySelector('.importan-places');
+const fruits = document.querySelectorAll(".fruits-container li");
+const fruit = document.querySelector('.fruits-container li');
+fruit.style.backgroundColor = "black";
+fruit.style.color = "white";
+for(const fruit of fruits){
+    // console.log(fruit.innerText);
 }
-
 fruitsTitle.innerText = "Foods I like most";
 
+
+singlePlace.innerText = "Netrakona";
+console.log(places)
 for(const place of places){
-    place.style.color = "tomato"
+    place.classList.add("text-center")
 }
+singelPlaceRemove.classList.remove("text-center");
+
+const sectionsH1 = document.querySelectorAll('section h1');
+for(const sectionH1 of sectionsH1){
+    sectionH1.style.color = "tomato"
+}
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = "5px";
+    section.style.borderRadius = "15px";
+    section.style.paddingLeft = "15px"
+}
+
+const color = document.getElementById('jalali');
+color.classList.add('yellow-bg');
+
 
 const changeColor = (newColor) =>{
     const element = document.getElementById('para');
@@ -19,5 +44,5 @@ const changeColor = (newColor) =>{
 document.querySelectorAll("button").forEach((button)=>{
     button.addEventListener("click", (event)=>{
         changeColor(event.target.textContent);
-    })
+    });
 });
